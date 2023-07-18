75657a3eab41da5ca4c38edbbcf01c7c9e1fe5a3
Xyx <739075802@qq.com>
2023-07-18 23:20:45 +0800

    [mod] support darwin

--- src/constant.js
+++ src/constant.js
@@ -5,3 +5,4 @@ export const validUrl = `https://{ip}/translate_a/element.js`;
 export const timeout = 1000;
 
 export const win32HostPath = `C:\\Windows\\System32\\drivers\\etc\\hosts`;
+export const macHostPath = `/etc/hosts`;
